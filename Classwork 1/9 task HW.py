9f91ba5104556f640143d457b0c7ad8cfab449bf
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-05-30 20:39:13 +0600

    deleted 1
    deleted 12

--- Classwork 1/9 task HW.py
+++
@@ -1,8 +0,0 @@
-number = 5678
-last_number = number % 10
-
-number = number // 10
-
-new_number = last_number * 10000 + number
-
-print(new_number)
--- Classwork 1/Строки и срезы classwork.py
+++
@@ -1,8 +0,0 @@
-Message = "Начало в %s часов" % 19
-#Начало в 19 часов
-
-hours, minutes = 21, 50
-template = "Заканчиваем в %s:%s"
-message = template % (hours, minutes)
-#Заканчиваем в 21:50
-
