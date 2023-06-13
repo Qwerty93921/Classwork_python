48919192bb157963f848a6cab0d603c653da3fe6
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-06-13 16:08:19 +0600

    asd
    asd

---
+++ Classwork from HW FILES/Строки/числа-01.py
@@ -0,0 +1,6 @@
+number = 56789
+last_digit = number % 10
+number = number // 10
+new_number = last_digit *10000 + number
+print(new_number)
+
---
+++ Classwork from HW FILES/Строки/числа-03.py
@@ -0,0 +1,12 @@
+user_in = input("Your salary is: ")
+salary = int(user_in)
+
+user_in = input("Credit payment per month: ")
+credit = int(user_in)
+
+user_in = input("Utility bill per month: ")
+utility = int(user_in)
+
+remains = salary - credit - utility
+
+print("Your remains:", remains)
