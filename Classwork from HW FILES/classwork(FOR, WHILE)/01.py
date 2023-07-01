a5947cfde8c1226da75e570dbb11c4304929da8b
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-07-01 22:26:16 +0600

    changes
    changes

---
+++ Classwork from HW FILES/classwork(FOR, WHILE)/01.py
@@ -0,0 +1,17 @@
+
+num = 1
+
+if num < 10:
+    print("меньше")
+else:
+    print("больше или равно")
+
+while num < 10:
+    num += 1
+    #if num == 5:
+    #    break
+    print("меньше,", num)
+else:
+    print("больше или равно")
+
+print("Конец")
