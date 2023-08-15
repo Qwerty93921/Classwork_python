7b25f687cc598f52eefd8317ba6d9960289e2caa
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-08-16 03:33:23 +0600

    asd
    asd

---
+++ Classwork from HW FILES/classwork OOP 1/02.py
@@ -0,0 +1,45 @@
+class Kettle():
+    temperature = 20
+    material = ""
+    color = ""
+    volume = 0
+    water = 0
+
+    def __init__(self, material, volume, color=None):
+        self.material = material
+        self.volume = volume
+        if color is not None:
+            self.color = color
+
+    def fill(self, liters):
+        self.water += liters
+        if self.water > self.volume:
+            extra = self.water - self.volume
+            print(f"Пролилось {extra} л лишней воды")
+            self.water = self.volume
+        print(f"Теперь в чайнике {self.water} л")
+
+    def pour(self, liters):
+        if liters > self.water:
+            print(f"Не могу вылить больше чем {self.water} л")
+            self.water = 0
+        else:
+            self.water -= liters
+        print(f"Теперь в чайнике {self.water} л")
+        
+    def boil(self):
+        self.temperature = 100
+        print("Wheeeeeeeeeeeeeeeeeeee!!!")
+
+
+my_kettle = Kettle("bamboo", 5, "yellow")
+my_kettle.fill(3)
+
+Kettle.color = "black"
+
+other_kettle = Kettle("iron", 0.7)
+print(other_kettle.color)
+
+other_kettle.fill(3.5)
+other_kettle.boil()
+other_kettle.pour(4)
---
+++ Classwork from HW FILES/classwork(OOP 4) polimorphism/cars.py
@@ -0,0 +1,48 @@
+class Car:
+    def __init__(self, model, speed, color, bak_max, consumption):
+        self.model = model
+        self.speed = speed
+        self.color = color
+        self.bak_max = bak_max
+        self.consumption = consumption / 100
+        self.fuel_volume = 5
+
+    def refuel(self, litres):
+        if self.fuel_volume + litres <= self.bak_max:
+            self.fuel_volume += litres
+            print("В баке:", self.fuel_volume, "л")
+        else:
+            over_fill = self.fuel_volume + litres - self.bak_max
+            self.fuel_volume = self.bak_max
+            print("Перелив:", over_fill, "л , бак полный")
+
+    def go(self, km):
+        fuel_waste = self.consumption * km
+        if self.fuel_volume >= fuel_waste:
+            time = km / self.speed
+            self.fuel_volume -= fuel_waste
+            print("Мы приехали за %.2f часов и в баке осталось %s литров" % (time, self.fuel_volume))
+        else:
+            print("На поезку надо %s л, а в баке всего %s" % (fuel_waste, self.fuel_volume))
+
+
+class PorsheCayenne(Car):
+    def __init__(self, color):
+        super().__init__('PorsheCayenne', 150, color, 60, 10)
+
+
+class Zhiguli(Car):
+    def __init__(self, color, extra_light):
+        super().__init__("Zhiguli", 100, color, 45, 15)
+        self.extra_light = extra_light
+
+
+
+my_car = PorsheCayenne("Красный")
+my_car.go(80)
+my_car.refuel(30)
+my_car.go(10)
+
+your_car = Zhiguli("Белый", "Синий")
+your_car.refuel(40)
+your_car.go(250)
