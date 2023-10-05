2be79965a5e37ed2be9b92a4ce20dd7919bff85c
Qwerty93921 <Artyomkoszxc21@mail.ru>
2023-10-05 21:41:28 +0600

    FORMS
    FORMS

---
+++ JavaScript/JS CLASSWORK/October 05(FORM)/October 05.js
@@ -0,0 +1,16 @@
+let testForm = document.myform;
+
+for (let i = 0; i < testForm.length; i++) {
+    console.log(testForm[i])
+}
+
+
+
+function sendData() {
+    
+}
+
+
+
+// testForm.hasAttribute(name);
+// console.log(testForm[0].hasAttribute('type'));
